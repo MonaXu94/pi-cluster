5ca32b453113900371905d2e0d5c89424205b271
MonaXu94 <g651271345@gmail.com>
2025-04-26 20:43:26 -0500

    fixed path

--- main.tf
+++ main.tf
@@ -1,10 +1,5 @@
 module "k3s_cluster" {
-  source = "./modules/k8s-cluster"
-
-  master_ips = [
-    "192.168.1.101", "192.168.1.102", "192.168.1.103", "192.168.1.104"
-  ]
-  cluster_name = "k3s-cluster"
+  source = "./modules/k3s-cluster"
 }
 
 module "monitoring" {
